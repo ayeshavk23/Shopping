686e8167d42f664d0b8e86081e686be30e91b2b5
ayeshavk23 <168327021+ayeshavk23@users.noreply.github.com>
2024-04-28 11:47:36 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Land from './Component/Assignment/Land'
+import './App'
+
+const App = () => {
+  return (
+    <>
+      <Land/>
+    </>
+  )
+}
+
+export default App
